3ca6dcb4a86baa72fe5a2dc84b2080521aef24bc
thunderfern <xyfernx@gmail.com>
2024-01-13 13:42:26 -0500

    created basic pygame layout

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+from pygame import *
 
+init()
+
+screen = display.set_mode()
+width, height = screen.get_size()
+clock = time.Clock()
+
+run = True
+
+while run:
+    display.flip()
+    clock.tick(60)
+
+quit()
